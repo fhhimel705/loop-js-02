adb4ff96cc03ebd0cf0ac31bb2389a85d754ff91
FH Himel <fhhimel17@gmail.com>
2023-12-11 03:32:10 +0600

    added-03

---
+++ numbers.js
@@ -0,0 +1,15 @@
+// you can add count either 1 or 2,3,4 as much as you want on need
+/*
+** you can get odd and even by changing i value 
+*/
+//  ----------even--------------
+for(i=0; i<10 ; i+=2){
+    console.log(i);
+}
+console.log("------------ends---------------");
+
+// ------------------odd----------------
+for (i = 1; i < 10; i += 2) {
+  console.log(i);
+}
+console.log("------------ends---------------");
---
+++ while.js
@@ -0,0 +1,14 @@
+// if you do any in the condition the loop wont even start
+let x = 0;
+while(x<10){
+    console.log(x);
+    x++;
+}
+console.log("------------ends---------------");
+// if you want 1-10 you have to start from 1
+let i = 1;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+console.log("------------ends---------------");
