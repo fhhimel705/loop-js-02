f4d68e3ada78cec33bac339cfa182a263d313230
FH Himel <fhhimel17@gmail.com>
2023-12-11 02:13:04 +0600

    first commit

---
+++ loop.js
@@ -0,0 +1,31 @@
+/*
+--------- 3 kinds of loop--------------
+***while
+***do while
+***for
+*/
+/*-------while-------*/
+let x = 0;
+while (x < 10) {
+  console.log(x);
+
+  x++;
+}
+console.log("------------ends---------------");
+/*----------
+---------do while
+---------*/
+let y = 0;
+do y++;
+while (y < 10);
+{
+  console.log(y);
+}
+console.log("------------ends---------------");
+/*----------------
+----------------for
+-----------------*/
+for (i = 0; i < 10; i++) {
+  console.log(i);
+}
+console.log("------------ends---------------");
