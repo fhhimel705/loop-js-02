a48084b4f25b2aa068aed3eaba339899592bad36
FH Himel <fhhimel17@gmail.com>
2023-12-11 04:05:43 +0600

    added-04

---
+++ array-loop.js
@@ -0,0 +1,12 @@
+// target : Display every element of an array
+let numbers = [1,2,3,4,5,6,7];
+
+for(i=0;i<numbers.length;i++){
+    let number = numbers[i];
+    console.log(number);
+}
+let names = ['halim' , 'bokkor' , 'sokhin' , 'julu'];
+for(i=0;i<names.length;i++){
+    let name = names[i];
+    console.log(name);
+}
---
+++ break.js
@@ -0,0 +1,25 @@
+// when at a point of a loop you want to stop you can simply give condition at which point you want the loop to stop.
+// there the two methods
+/**
+ * you can break it after fulfilling the condition (RECOMMENDED)
+ */
+// for (i = 0; i < 10; i++) {
+//   console.log(i);
+//   if (i > 4) {
+//     break;
+//   }
+// }
+let items = ['glass' , 'indicator' , 'filter' , 'tier' , 'seat'];
+for(i=0;i<items.length;i++){
+ let item = items[i];
+ console.log(item);
+}
+/**
+ * you can break it befor fulfilling the condition
+ */
+// for (i = 0; i < 10; i++) {
+//   if (i > 4) {
+//     break;
+//   }
+//   console.log(i);
+// }
